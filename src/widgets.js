6048c772777359f97f2b571b66bedf4a368137f3
Shashi Gowda <shashigowda91@gmail.com>
2014-06-06 23:50:17 +0530

    Changed structure of widget, added label field.

--- src/widgets.js
+++ src/widgets.js
@@ -11,42 +11,47 @@
 
     // A widget must expose an id field which identifies it to the backend,
     // an elem attribute which is will be added to the DOM, and
-    // a getState method which returns the value to be sent to the backend
+    // a getState() method which returns the value to be sent to the backend
+    // a sendUpdate() method which sends its current value to the backend
     var Widget = {
 	id: undefined,
 	elem: undefined,
+	label: undefined,
 	getState: function () {
 	    return this.elem.value;
+	},
+	sendUpdate: function () {
 	}
     };
 
     var Slider = function (typ, id, init) {
-	var attr = { type: "range",
+	var attr = { type:  "range",
 		     value: init.value,
 		     min:   init.min,
-		     max:   init.max };
-	elem = createElem("input", attr);
+		     max:   init.max,
+		     step:  init.step },
+	    elem = createElem("input", attr),
+	    self = this;
 
-	elem.onchange = InputWidgets.updateSender(this);
+	elem.onchange = function () {
+	    console.log("state change", self.getState());
+	    self.sendUpdate();
+	}
 
 	this.id = id;
 	this.elem = elem;
+	this.label = init.label;
+
+	InputWidgets.commInitializer(this); // Initialize communication
     }
     Slider.prototype = Widget;
 
     var InputWidgets = {
 	Slider: Slider,
-	setCommHandler: function (f) {
-	    // TODO: wrap f to throttle
-	    InputWidgets._communicate = f;
-	},
-	updateSender: function (widget) {
-	    return function () {
-		InputWidgets._communicate(widget);
-	    };
-	},
-	_communicate: function (widget) {
-	    console.log("State update (", widget.id, "):", widget.getState());
+	// a central way to initalize communication
+	// for widgets.
+	commInitializer: function (widget) {
+	    widget.sendUpdate = function () {};
 	}
     };
 
