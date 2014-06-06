9313a708389a4951b3c49668b10ea0daabe7eb23
Shashi Gowda <shashigowda91@gmail.com>
2014-06-06 17:43:18 +0530

    IJulia specific setup

--- src/ijulia.js
+++ src/ijulia.js
@@ -1,28 +1,30 @@
 (function (IPython, $) {
     var comm_manager = IPython.notebook.kernel.comm_manager;
+    console.log(comm_manager);
 
-    $([IPython.events]).on('output_appended.OutputArea', function (event, type, value, md, toinsert) {
-	if (md && md.reactive) {
-	    toinsert.addClass("signal-" + md.comm_id);
-	    toinsert.data("type", type);
-	}
+    $([IPython.events]).on(
+	'output_appended.OutputArea', function (event, type, value, md, toinsert) {
+    	if (md && md.reactive) {
+    	    toinsert.addClass("signal-" + md.comm_id);
+    	    toinsert.data("type", type);
+    	}
     });
 
-    comm_manager.register_target("signal", function (comm) {
-	console.log(comm);
-	comm.on_msg(function (msg) {
-	    var val = msg.content.data.value;
-	    $(".signal-" + comm.comm_id).each(function() {
-		var self = this;
-		var type = $(this).data("type");
-		if (val[type]) {
-		    var oa = new IPython.OutputArea();
-		    var toinsert = IPython.OutputArea.append_map[type].apply(
-			oa, [val[type], {}, $("<div/>")]
-		    );
-		    $(self).html(toinsert.html());
-		}
-	    });
-	});
+    comm_manager.register_target("Signal", function (comm) {
+    	comm.on_msg(function (msg) {
+	    console.log("MSG", msg);
+    	    var val = msg.content.data.value;
+    	    $(".signal-" + comm.comm_id).each(function() {
+    		var self = this;
+    		var type = $(this).data("type");
+    		if (val[type]) {
+    		    var oa = new IPython.OutputArea();
+    		    var toinsert = IPython.OutputArea.append_map[type].apply(
+    			oa, [val[type], {}, $("<div/>")]
+    		    );
+    		    $(self).html(toinsert.html());
+    		}
+    	    });
+    	});
     });
 })(IPython, jQuery);
--- src/ijulia_setup.jl
+++ src/ijulia_setup.jl
@@ -2,17 +2,16 @@
 # Assume IJulia is running
 
 
-const ijulia_js = readall(joinpath(dirname(Base.source_path()), "ijulia.js"))
+const ijulia_js  = readall(joinpath(dirname(Base.source_path()), "ijulia.js"))
 
 try
-    display("text/html", """<script charset="utf-8">$(_js)</script>""")
+    display("text/html", """<script charset="utf-8">$(ijulia_js)</script>""")
 catch
 end
 
-using Main.IJulia
-using Main.IJulia.CommManager
+using  Main.IJulia
+using  Main.IJulia.CommManager
 import Main.IJulia: metadata
-using InputWidgets
 
 const comms = Dict{Int, Comm}()
 const signals = Dict{String, Signal}()
@@ -30,27 +29,23 @@ end
 function Main.IJulia.metadata(x :: Signal)
     if ~haskey(comms, x.id)
         # One Comm channel per signal object
-        comm = Comm("signal")
+        comm = Comm("Signal")
 
         comms[x.id] = comm   # Backend -> Comm
         signals[comm.id] = x # Comm -> Backend
 
-        # prevent resending first time?
+        # prevent resending the first time?
         lift(v -> send_update(comm, v), x)
-        if isa(x, Input)
-            # lift(recv_update, inbound_signal(x))
-        end
     else
         comm = comms[x.id]
     end
     return ["reactive"=>true, "comm_id"=>comm.id]
 end
 
-# Ideally, overriding display_dict is not necessary
 Main.IJulia.display_dict(x :: Signal) =
     Main.IJulia.display_dict(x.value)
 
-
+# Render the value of a signal.
 mimewritable(m :: MIME, s :: Signal) =
     mimewritable(m, s.value)
 
