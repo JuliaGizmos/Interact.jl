a4239a755bfffe982aff0c998aaed7db91261005
Shashi Gowda <shashigowda91@gmail.com>
2014-12-21 23:15:50 +0530

    Fix Dicts for v0.3

--- src/IJulia/statedict.jl
+++ src/IJulia/statedict.jl
@@ -1,10 +1,10 @@
 statedict(s::Union(Slider, Progress)) =
-    Dict(:value=>s.value,
+    @compat Dict(:value=>s.value,
          :min=>first(s.range),
          :step=>step(s.range),
          :max=>last(s.range))
 
 # when we say value to javascript, it really means value label
 statedict(d::Options) =
-    Dict(:value_name=>d.value_label,
+    @compat Dict(:value_name=>d.value_label,
          :value_names=>collect(keys(d.options)))
