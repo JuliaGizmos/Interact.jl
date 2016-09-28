ed4f52574f71c1c5f6f2a4c797ec61ceaf3aced9
Shashi Gowda <shashigowda91@gmail.com>
2016-09-28 16:14:51 +0530

    fix #111

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,15 @@
 using Compat
 
-if isa(Pkg.installed("IJulia"), VersionNumber)
-    using IJulia
-    python = strip(readline(IJulia.jupyter), ['\n',' ', '#','!'])
+function main()
+
+    @static if is_linux() || is_apple()
+        python = strip(readline(readstring(`which $(IJulia.jupyter)`)|>strip), ['\n',' ', '#','!'])
+    elseif is_windows()
+        warn("cannot determine jupyter's python path in Windows, bailing.")
+        return
+    end
     ipywver = readstring(`$python -c 'import ipywidgets; print ipywidgets.__version__'`) |> strip |> VersionNumber
+
     info("ipywidgets version found: $ipywver")
     if ipywver < v"5.0.0"
         warn("""This version of Interact requires ipywidgets > 5.0 to work correctly.
@@ -11,6 +17,11 @@ if isa(Pkg.installed("IJulia"), VersionNumber)
     else
         info("A compatible version of ipywidgets was found. All good.")
     end
+end
+
+if isa(Pkg.installed("IJulia"), VersionNumber)
+    using IJulia
+    main()
 else
     warn("""IJulia is not installed. run Pkg.build("Interact")
             once you install IJulia to use Interact inside IJulia""")
