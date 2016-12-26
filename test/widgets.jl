dbf4e7685b7ab662707b449f3f9c6f6ec7b860b8
Shashi Gowda <shashigowda91@gmail.com>
2016-12-26 12:47:33 +0530

    update tests

--- test/widgets.jl
+++ test/widgets.jl
@@ -11,19 +11,25 @@ import Reactive: value
 
 
 sliderWidget = slider(1:5)
-
-@test Dict(:range=>1:5,
-           :value=>3,
-           :readout_format=>"d",
-           :continuous_update=>true) == Interact.statedict(sliderWidget)
-@test 3 == value(signal(sliderWidget))
-@test 1 == parse_msg(sliderWidget, 1)
-@test 0 == parse_msg(sliderWidget, 0)
-
-
 checkboxWidget = checkbox(false)
-@test Dict(:value=>false) == Interact.statedict(checkboxWidget)
-@test false == value(signal(checkboxWidget))
+@testset "statedict" begin
 
-@test 1 == parse_msg(checkboxWidget, 1)
-@test 0 == parse_msg(checkboxWidget, 0)
+    @test Dict(:range=>1:5,
+               :value=>3,
+               :readout_format=>"d",
+               :readout=>true,
+               :orientation=>"horizontal",
+               :continuous_update=>true) == Interact.statedict(sliderWidget)
+    @test Dict(:value=>false) == Interact.statedict(checkboxWidget)
+end
+
+@testset "parse" begin
+    @test 3 == value(signal(sliderWidget))
+    @test false == value(signal(checkboxWidget))
+
+    @test 1 == parse_msg(sliderWidget, 1)
+    @test 0 == parse_msg(sliderWidget, 0)
+
+    @test 1 == parse_msg(checkboxWidget, 1)
+    @test 0 == parse_msg(checkboxWidget, 0)
+end
