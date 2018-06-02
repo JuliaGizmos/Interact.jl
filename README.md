c4254dfa43f1f3446f8c3eb3d04cac42aaf2374a
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-02 12:18:34 +0100

    port to interactbase

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 
 Interact.jl allows you to use interactive widgets such as sliders, dropdowns and checkboxes to play with your Julia code.
 
-![Screenshot](http://i.imgur.com/xLWjmNb.png)
 
 ## Getting Started
 
@@ -12,14 +11,6 @@ To install Interact, run the following command in the Julia REPL:
 ```{.julia execute="false"}
 Pkg.add("Interact")
 ```
-
-**Note:** Interact only works with IPython/Jupyter version 3.0 or higher.
-
-To start using it in an IJulia notebook, include it:
-```{.julia execute="false"}
-using Interact
-```
-[`GtkInteract`](https://github.com/jverzani/GtkInteract.jl) provides Gtk support for Interact, letting you use tools like `Winston` for plotting.
 
 ## Example notebooks
 
@@ -29,21 +20,3 @@ The best way to learn to use the interactive widgets is to try out the example n
 using IJulia
 notebook()
 ```
-
-## Troubleshooting
-
-### Jupyter/IPython version support
-
-Interact needs IJulia to be running on Jupyter/IPython 3.x or Jupyter 4.x.
-For Jupyter 4.x, the Python packages `jupyter` and `ipywidgets` must be installed.
-
-### `ipywidgets` version
-
-Interact relies on the `ipywidgets` python package which is an add-on to Jupyter that provides the widgets. The 5.x version of `ipywidgets` is installed by default by IJulia as of now. It might be the case that you have version 4.x of `ipywidgets` in which case you will need to use the `ipywidgets-4` branch of `Interact` due to backwards-incompatible changes. To do this, simply run `Pkg.checkout("Interact", "ipywidgets-4")`. You can run
-
-```python
-import ipywidgets
-ipywidgets.__version__
-```
-
-in the python shell used by Jupyter to find out which version of ipywidgets you have. (Pkg.build("Interact") should also tell you which version of ipywidgets you have)
--- src/Interact.jl
+++ src/Interact.jl
@@ -2,141 +2,23 @@ __precompile__()
 
 module Interact
 
-using Reactive, Compat, DataStructures
+using Reexport
 
-import Base: mimewritable
-export signal, Widget, InputWidget
+@reexport using InteractBase
 
-# A widget
-@compat abstract type Widget end
+import InteractUIkit, InteractBulma
 
-# A widget that gives out a signal of type T
-@compat abstract type InputWidget{T}  <: Widget end
+const themes = Dict(
+    :uikit => InteractUIkit.UIkit(),
+    :bulma => InteractBulma.Bulma()
+)
 
-signal(w::InputWidget) = w.signal
-signal(x::Signal) = x
-
-function statedict(w)
-    msg = Dict()
-    attrs = @compat fieldnames(w)
-    for n in attrs
-        if n in [:signal, :label]
-            continue
-        end
-        msg[n] = getfield(w, n)
-    end
-    msg
+function InteractBase.settheme!(s::Symbol)
+    (s in keys(themes)) || error("Theme $s is not supported")
+    settheme!(themes[s])
 end
 
-function viewdict(w::Widget)
-    Dict()
+empty!(InteractBase.backend)
+settheme!(:bulma)
+
 end
-
-# Convert e.g. JSON values into Julia values
-parse_msg{T <: Number}(::InputWidget{T}, v::AbstractString) = parse(T, v)
-parse_msg(::InputWidget{Bool}, v::Number) = v != 0
-parse_msg{T}(::InputWidget{T}, v) = convert(T, v)
-
-"""
-update the view of a widget.
-child packages need to override this function
-"""
-function update_view end
-
-
-function error_handler(sig, value, err)
-    Reactive.print_error(sig, value, err, open("/tmp/Interact.log", "a"))
-end
-
-function recv_msg{T}(widget::InputWidget{T}, val)
-    # Hand-off received value to the signal graph
-    parsed = parse_msg(widget, val)
-    widget.value = parsed
-    if signal(widget).value != parsed || isa(widget, Button)
-        #push only changed values, but for Buttons we always push
-        push!(signal(widget), parsed)
-    end
-    if val != parsed
-        println(STDERR, "val != parsed: <val>$val</val> <parsed>$parsed</parsed>")
-        update_view(widget)
-    end
-end
-
-uuid4() = string(Base.Random.uuid4())
-
-const id_to_widget = Dict{AbstractString, InputWidget}()
-const widget_to_id = Dict{InputWidget, AbstractString}()
-
-function register_widget(w::InputWidget)
-    if haskey(widget_to_id, w)
-        return widget_to_id[w]
-    else
-        id = uuid4()
-        widget_to_id[w] = id
-        id_to_widget[id] = w
-        return id
-    end
-end
-
-function get_widget(id::AbstractString)
-    if haskey(id_to_widget, id)
-        return id_to_widget[id]
-    else
-        warn("Widget with id $(id) does not exist.")
-    end
-end
-
-include("widgets.jl")
-include("compose.jl")
-include("manipulate.jl")
-
-const ijulia_setup_path = joinpath(dirname(@__FILE__), "IJulia", "setup.jl")
-const ijulia_setup_path_old = joinpath(dirname(@__FILE__), "IJulia", "setup_old.jl")
-const ipywidgets_version = joinpath(dirname(@__FILE__), "..", "deps", "widgets_version")
-
-function __init__()
-    if isdefined(Main, :IJulia)
-        if haskey(ENV, "WIDGETS_VERSION")
-            v = VersionNumber(ENV["WIDGETS_VERSION"])
-        elseif isfile(ipywidgets_version)
-            v = VersionNumber(strip(readline(ipywidgets_version)))
-        else
-            v = v"3.0.0"
-        end
-        if v >= v"3.0.0"
-            include(ijulia_setup_path)
-        else
-            include(ijulia_setup_path_old)
-        end
-    end
-end
-
-"""
-Interact.jl allows you to use interactive widgets such as sliders, dropdowns and checkboxes to play with your Julia code.
-
-Basic widgets:
-
-- `slider(1:10)` creates a slider widget with the specified range of values
-- `checkbox(false)` creates a check box
-- `togglebutton(false)` creates a toggle button
-- `button()` creates a button
-
-Option (selection) widgets:
-
-- `dropdown(["one", "two", "three"])` creates a drop-down widget
-- `togglebuttons(["one", "two", "three"])` creates a toggle button group
-- `radiobuttons(["one", "two", "three"])` creates a radio button group
-
-Text boxes:
-
-- `textbox(value)` creates a box accepting string or numeric text input
-- `textarea` creates a box for extended text input
-
-Output widgets:
-
-- `latex`
-- `progress`
-"""
-Interact
-
-end # module
