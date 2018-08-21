bafdbadacca4904ed040544091bdd8858a38ed29
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-08-21 19:56:49 +0100

    test on julia 1

--- docs/make.jl
+++ docs/make.jl
@@ -20,7 +20,7 @@ makedocs(
 deploydocs(
     repo = "github.com/JuliaGizmos/Interact.jl.git",
     target = "build",
-    julia  = "0.6",
+    julia  = "0.7",
     osname = "linux",
     deps   = nothing,
     make   = nothing
