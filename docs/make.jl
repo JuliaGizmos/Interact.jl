75709fecdc8c146b88988dc718b29e7eebc4df2c
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-08-21 19:59:15 +0100

    typo

--- docs/make.jl
+++ docs/make.jl
@@ -20,7 +20,7 @@ makedocs(
 deploydocs(
     repo = "github.com/JuliaGizmos/Interact.jl.git",
     target = "build",
-    julia  = "0.7",
+    julia  = "1.0",
     osname = "linux",
     deps   = nothing,
     make   = nothing
