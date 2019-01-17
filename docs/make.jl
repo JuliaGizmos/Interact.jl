382a606cfe90579411dd5677525a7b1d9d1717ed
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-01-17 20:18:01 +0100

    switch to new documenter (#286)
    * switch to new documenter
    
    * fix path
    
    * document modifiers
    
    * typo

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,6 @@ src = joinpath(@__DIR__, "src")
 Literate.markdown(joinpath(src, "tutorial.jl"), src, codefence = "```julia" => "```")
 
 makedocs(
-    format = :html,
     sitename = "Interact",
     authors = "JuliaGizmos",
     pages = [
@@ -11,6 +10,7 @@ makedocs(
         "Observables" => "observables.md",
         "Widgets" => "widgets.md",
         "Custom widgets" => "custom_widgets.md",
+        "Modifiers" => "modifiers.md",
         "Layout" => "layout.md",
         "Deploying the web app" => "deploying.md",
         "Tutorial" => "tutorial.md",
@@ -19,9 +19,4 @@ makedocs(
 
 deploydocs(
     repo = "github.com/JuliaGizmos/Interact.jl.git",
-    target = "build",
-    julia  = "1.0",
-    osname = "linux",
-    deps   = nothing,
-    make   = nothing
 )
---
+++ docs/src/modifiers.md
@@ -0,0 +1,8 @@
+# Modifiers
+
+Interact provides some modifiers to change an existing widget (or return a tranformed version of the widget):
+
+```@docs
+InteractBase.tooltip!
+onchange
+```
