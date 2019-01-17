619038a2f0438376f6870e1fd466e8e68c51687e
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-01-17 17:16:45 +0000

    fix path

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using Documenter, Interact, Literate
 src = joinpath(@__DIR__, "src")
-Literate.markdown(joinpath(src, "notebooks", "tutorial.jl"), src, codefence = "```julia" => "```")
+Literate.markdown(joinpath(src, "tutorial.jl"), src, codefence = "```julia" => "```")
 
 makedocs(
     sitename = "Interact",
