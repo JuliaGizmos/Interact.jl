549e6762a662f6a13b8faf382ec5b6f05d0af958
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-18 14:23:03 +0000

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,9 @@ settheme!(:nativehtml)
 @test gettheme() == InteractBase.NativeHTML()
 settheme!(:bulma)
 @test gettheme() == Interact.Bulma()
+
+@test isfile(Interact.main_css)
+@test isfile(Interact.main_interactbulma_css)
+
+@test first(eachline(Interact.main_css))[1:20] == "/*! bulma.io v0.7.4 "
+@test first(eachline(Interact.main_interactbulma_css))[1:20] == ".interactbulma{/*! b"
