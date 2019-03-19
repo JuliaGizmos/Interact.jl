6ecd2e93a613d9730cc5b5cb77a7e307888ce549
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-19 12:23:29 +0000

    switch to interact-widget css

--- deps/build.jl
+++ deps/build.jl
@@ -4,8 +4,8 @@ const _pkg_assets = joinpath(_pkg_root,"assets")
 !isdir(_pkg_assets) && mkdir(_pkg_assets)
 
 deps = [
-    "https://www.gitcdn.xyz/repo/piever/InteractResources/v0.2.0/bulma/main.min.css",
-    "https://www.gitcdn.xyz/repo/piever/InteractResources/v0.2.0/bulma/main_interactbulma.min.css",
+    "https://www.gitcdn.xyz/repo/piever/InteractResources/master/bulma/main.min.css",
+    "https://www.gitcdn.xyz/repo/piever/InteractResources/master/bulma/main_confined.min.css",
 ]
 
 for dep in deps
--- test/runtests.jl
+++ test/runtests.jl
@@ -12,7 +12,7 @@ settheme!(:bulma)
 @test isfile(Interact.main_interactbulma_css)
 
 @test first(eachline(Interact.main_css))[1:20] == "/*! bulma.io v0.7.4 "
-@test first(eachline(Interact.main_interactbulma_css))[1:20] == ".interactbulma{/*! b"
+@test first(eachline(Interact.main_interactbulma_css))[1:20] == ".interact-widget{/*!"
 
 @test length(InteractBase.libraries(Interact.Bulma())) == 3
 @test all(isfile, InteractBase.libraries(Interact.Bulma()))
