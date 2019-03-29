bf1d6ffcf6e4835568922a8903a39382dd3f0f4b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-29 18:51:58 +0000

    build based on a87abf3

--- dev/search_index.js
+++ dev/search_index.js
@@ -357,7 +357,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Widgets",
     "title": "Widgets.button",
     "category": "function",
-    "text": "button(content... = \"Press me!\"; value=0)\n\nA button. content goes inside the button. Note the button content supports a special clicks variable, that gets incremented by 1 with each click e.g.: button(\"clicked {{clicks}} times\"). The clicks variable is initialized at value=0\n\n\n\n\n\n"
+    "text": "button(content... = \"Press me!\"; value=0)\n\nA button. content goes inside the button. Note the button content supports a special clicks variable, that gets incremented by 1 with each click e.g.: button(\"clicked {{clicks}} times\"). The clicks variable is initialized at value=0. Given a button b, b[\"is-loading\"] defines whether the button is in a loading state (spinning wheel). Use b[\"is-loading\"][]=true or b[\"is-loading\"][]=false respectively to display or take away the spinner.\n\n\n\n\n\n"
 },
 
 {
